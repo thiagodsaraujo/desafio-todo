d33b198d7c1fb0fc6d84e980572425181f0cde7f
Thiago Araujo <thiago.dos.araujo@maisunifacisa.com.br>
2024-02-17 14:56:22 -0300

    Configurações etc

---
+++ src/main/java/desafioapitodo/DesafioApiToDoApplication.java
@@ -0,0 +1,13 @@
+package desafioapitodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioApiToDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/desafioapitodo/DesafioApiToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package desafioapitodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
